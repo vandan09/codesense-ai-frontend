753ee801963be8688f60572537d4f8e9023c6c0a
vandan09 <vandan8154@gmail.com>
2025-06-28 00:02:40 +0530

    base url point to prod

--- src/api/apiConfig.jsx
+++ src/api/apiConfig.jsx
@@ -1,4 +1,5 @@
-// src/api/apiConfig.js
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://codesense-ai-43kd.onrender.com/api";
+
 
 export default BASE_URL;
